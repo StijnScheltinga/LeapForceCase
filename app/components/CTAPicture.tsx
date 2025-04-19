5b9987a7d76e5032ecdc9999e0d44bcc25f251db
StijnScheltinga <stijnskoop@gmail.com>
2025-04-19 13:39:41 +0200

    initial desktop version

---
+++ app/components/CTAPicture.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Image from 'next/image'
+
+const CTAPicture = () => {
+	return (
+		<div className='relative h-[515px] w-full bg-[url(/images/Image.png)] bg-cover bg-left bg-no-repeat rounded-[4px]'>
+			<div className='absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-[#141414] opacity-[.80] to-transparent rounded-[4px]'/>
+			<div className='absolute bottom-4 right-4 left-4 z-10 items-center flex justify-between'>
+				<div>
+					<h3 className='font-ttcommons font-[600] text-[28px] leading-[32px] tracking-wide text-white mb-2'>
+						Title
+					</h3>
+					<p className='font-ttcommons font-[450] text-[19px] leading-[24px] text-[#FAFAFA50]'>
+						Supporting text about the call-to-action goes here.
+					</p>
+				</div>
+				<a href="htps://google.com">
+					<Image className='absolute bottom-0 right-0'
+						src="icons/Arrow Button.svg"
+						alt="arrow icon"
+						width="56"
+						height="56"
+					/>
+				</a>
+			</div>
+		</div>
+	)
+}
+
+export default CTAPicture
---
+++ app/components/CTASection.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Image from 'next/image';
+import CTAPicture from './CTAPicture';
+
+const CTASection = () => {
+	return (
+		<section className="bg-[#FFFCFA] max-w-[1440px] flex flex-col gap-16 mx-auto px-[64px] pt-[64px] pb-[96px]">
+			<div className="max-w-[864px] mx-auto flex flex-col gap-6">
+				<div className="max-w-[642px] mx-auto flex flex-col items-center gap-2">
+					<p className="font-ttcommons font-[450] text-[#39494D] text-[15px] leading-[16px] tracking-[0.02em] text-center max-w-[642px] mx-auto">Label</p>
+					<h1 className="font-ttcommons font-[600] text-[#212121] text-[40px] leading-[48px] text-center">
+						Mandatory title about the{' '}
+						<span className="font-gtalpina whitespace-nowrap font-[300] text-[42px]">
+							call-to-action{' '}
+						</span>
+						here
+					</h1>
+				</div>
+				<p className="font-ttcommons font-[450] text-[19px] leading-[24px] text-custom-gray-80 text-center">
+					Optional body copy goes here.
+				</p>
+				<div className="font-ttcommons font-[450] text-[19px] leading-[24px] flex flex-row gap-1 justify-center text-custom-orange">
+					<a href="#" className='hover:text-[#A6433A] hover:underline'>
+						Optional button
+					</a>
+					<Image
+						src="icons/Arrow Right.svg"
+						alt="arrow icon"
+						width="24"
+						height="24"
+						/>
+				</div>
+			</div>
+			<div className='flex flex-row gap-[24px]'>
+				<CTAPicture />
+				<CTAPicture />
+			</div>
+		</section>
+	)
+}
+
+export default CTASection
